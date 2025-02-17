889a6477ae3448d8b4f14c89f3d5b87228be81ab
SolidData <david.tiefenthaler@web.de>
2025-02-17 23:11:59 +0100

    refactor: update import paths and add module docstring for clarity

--- packages/lit-auto-encoder/tests/test_train_autoencoder.py
+++ packages/lit-auto-encoder/tests/test_train_autoencoder.py
@@ -1,7 +1,8 @@
-from uv_datascience_project_monorepo_template.train_autoencoder import train_litautoencoder
+from lit_auto_encoder.train_autoencoder import train_litautoencoder
 
 
 def test_train_litautoencoder() -> None:
+    """Test training process."""
     encoder, decoder, is_model_trained = train_litautoencoder()
     assert encoder is not None
     assert decoder is not None
